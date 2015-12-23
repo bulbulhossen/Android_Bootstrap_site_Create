0fc832ce1938d254f4679909cae65965d2238dbe
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-24 05:42:59 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/android_bootstrap_site_create/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bulbulhossen.android_bootstrap_site_create;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView;
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/car/index.html");
+
+    }
+}
