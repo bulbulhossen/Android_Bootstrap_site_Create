19582170cda1ef6bc15b6990ccd3c16d006fd354
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-24 15:09:12 +0600

    Web view code add

--- app/src/main/java/com/bulbulhossen/android_bootstrap_site_create/MainActivity.java
+++ app/src/main/java/com/bulbulhossen/android_bootstrap_site_create/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //For web view show java code !!!
+
         WebView webView;
         webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
